93bcc04738e1df29f946689cd22216b1dc60c01d
VINODvoid <iamlearner.v@gmail.com>
2023-12-03 12:43:47 +0530

    Add React and Vite configuration files

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function App() {
+  const [counter , setCounter] = useState(0)
+  // let counter = 15
+  const addvalue = () => {
+    // counter += 1;
+    if(counter >=20)
+    {
+      setCounter(20)
+    
+    }
+    else
+    setCounter(counter +1)
+    
+  }
+  const removevalue = () => {
+    if(counter <= 0)
+    {
+      setCounter(0)
+    }
+    else{
+      setCounter(counter -1)
+    }
+    
+    
+
+  }
+  return (
+    <>
+     <h1> Dynamic Counter  </h1>
+      
+      <button
+        onClick={addvalue}>+</button>
+      <br />
+      <hr />
+      <br />
+      <h2>
+        Counter Value is {counter}</h2>
+      <hr />
+      <br />
+      
+      <button onClick={removevalue}>-</button>
+
+    </>
+
+  )
+}
+
+export default App;
